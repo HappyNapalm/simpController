8f853df8beadc71314704ffc169ce3b43c7e6093
Church <maevestring@gmail.com>
2020-10-03 15:51:20 -0700

    verified that the adafruit library works on adafruit brand A-LEDs. Moving on to testing Liteon brand.

--- Arduino/Software/Hello_NeoPixels/Hello_NeoPixels.ino
+++ Arduino/Software/Hello_NeoPixels/Hello_NeoPixels.ino
@@ -1,28 +1,3 @@
-//#include <Adafruit_CircuitPlayground.h>
-//
-//void setup() {
-//  CircuitPlayground.begin();
-//}
-//
-//void loop() {
-//  CircuitPlayground.clearPixels();
-//
-//  delay(500);
-//
-//  CircuitPlayground.setPixelColor(0, 255,   0,   0);
-//  CircuitPlayground.setPixelColor(1, 128, 128,   0);
-//  CircuitPlayground.setPixelColor(2,   0, 255,   0);
-//  CircuitPlayground.setPixelColor(3,   0, 128, 128);
-//  CircuitPlayground.setPixelColor(4,   0,   0, 255);
-//  
-//  CircuitPlayground.setPixelColor(5, 0xFF0000);
-//  CircuitPlayground.setPixelColor(6, 0x808000);
-//  CircuitPlayground.setPixelColor(7, 0x00FF00);
-//  CircuitPlayground.setPixelColor(8, 0x008080);
-//  CircuitPlayground.setPixelColor(9, 0x0000FF);
-// 
-//  delay(5000);
-//}
 #include <Adafruit_NeoPixel.h>
 
 //define NeoPixel Pin and Number of LEDs
@@ -46,6 +21,14 @@ void loop() {
   delay(1000);
   // set pixel to off, delay(1000)
   strip.setPixelColor(0, 0, 0, 0);
+  strip.show();
+  delay(1000);
+    // set pixel to red, delay(1000)
+  strip.setPixelColor(1, 0, 255, 0);
+  strip.show();
+  delay(1000);
+  // set pixel to off, delay(1000)
+  strip.setPixelColor(1, 0, 0, 0);
   strip.show();
   delay(1000);
 }
